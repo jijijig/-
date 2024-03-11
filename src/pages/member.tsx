622a44e3970498bdf452d24d629c7d19ecf886aa
Hueeng <zzxx373014@gmail.com>
2024-03-11 10:51:52 +0900

    #2 멤버 틀 잡기

--- src/pages/member.tsx
+++ src/pages/member.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 export default function member() {
+  const members = [
+    {
+      id: 1,
+      name: "멤버 1",
+      description: "멤버 1 설명",
+      imageUrl: "멤버1 이미지",
+    },
+    {
+      id: 2,
+      name: "멤버 2",
+      description: "멤버 2 설명",
+      imageUrl: "멤버2 이미지",
+    },
+  ];
   return (
-    <div>member</div>
-  )
+  
+    <PageContainer>
+    <h2>팀 멤버 소개</h2>
+    <MemberList>
+      {members.map(member => (
+        <MemberCard key={member.id}>
+          <MemberImage src={member.imageUrl} alt={member.name} />
+          <MemberName>{member.name}</MemberName>
+          <MemberDescription>{member.description}</MemberDescription>
+        </MemberCard>
+      ))}
+    </MemberList>
+  </PageContainer>
+    )
 }
+const PageContainer = styled.div`
+  text-align: center;
+`;
+
+const MemberList = styled.div`
+  display: flex;
+  text-align: center;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MemberCard = styled.div`
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+`;
+
+const MemberImage = styled.img`
+  width: 100%;
+  border-radius: 8px;
+`;
+
+const MemberName = styled.h3`
+  margin: 10px 0;
+`;
+
+const MemberDescription = styled.p`
+  color: #666;
+`;
+
