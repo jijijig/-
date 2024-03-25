718cf3bec81b70d3b3f124ba9dcb293e0ab3a58f
Hueeng <zzxx373014@gmail.com>
2024-03-26 00:15:51 +0900

    .

---
+++ src/pages/home.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import HomeTitle from "@/Components/Home/HomeTitle";
+import HomeSub from "@/Components/Home/Homesub";
+
+export default function home() {
+  return (
+    <Warpper>
+        <div className="homeblock1">
+        <HomeTitle/>
+        </div>
+        <div className="homeblock2">
+        <HomeSub/>
+        </div>
+
+        
+    </Warpper>
+  )
+}
+const Warpper = styled.div`
+margin: 67px 0 0 10% ;
+padding: 0;
+width: 80vw;
+height: 60vh;
+background-color: #f5f5f5;
+display: flex;
+justify-content: space-around;
+.homeblock1{
+    width: 50%;
+    height: 100%;
+    background-color: green;
+    }
+.homeblock2{
+    width: 50%;
+    height: 100%;
+    background-color: yellow;
+    }
+
+`;
+
