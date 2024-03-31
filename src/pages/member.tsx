432a84c25acdfcd61d01725e978c9a69f94d9019
LeeHueeng <zzxx373014@gmail.com>
2024-03-31 21:57:11 +0900

    #4 멤버소개 업데이트 완료

--- src/pages/member.tsx
+++ src/pages/member.tsx
@@ -1,55 +1,78 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export default function member() {
   const members = [
     {
       id: 1,
       name: "이현우",
-      description: "팀장 & 프론트엔드 개발자",
+      description: "PM&Web Developer",
       imageUrl: "path/member/member1.jpeg",
     },
     {
       id: 2,
       name: "최민우",
-      description: "백엔드 개발자",
+      description: "Back-end Developer",
+      imageUrl: "path/member/member2.jpeg",
+    },
+    {
+      id: 3,
+      name: "윤지원",
+      description: "PM&Ui/Ux Designer",
       imageUrl: "path/member/member2.jpeg",
     },
   ];
   return (
-  
     <PageContainer>
-    <h2>팀 멤버 소개</h2>
-    <p>최악의 상황에서도 동작하는 사이트를 만들자는 목표를 가지고 제작합니다.</p>
-    <MemberList>
-      {members.map(member => (
-        <MemberCard key={member.id}>
-          <MemberImage src={member.imageUrl} alt={member.name} />
-          <MemberName>{member.name}</MemberName>
-          <MemberDescription>{member.description}</MemberDescription>
-        </MemberCard>
-      ))}
-    </MemberList>
-  </PageContainer>
-    )
+      <p className="mainp">Our Team</p>
+      <h2>팀 멤버 소개</h2>
+      <p className="explanation">
+        최악의 상황에서도 동작하는 사이트를 만들자는 목표를 가지고 제작합니다.
+      </p>
+      <MemberList>
+        {members.map((member) => (
+          <MemberCard key={member.id}>
+            <MemberImage src={member.imageUrl} alt={member.name} />
+            <div className="explanationbox">
+              <MemberName>{member.name}</MemberName>
+              <MemberDescription>{member.description}</MemberDescription>
+            </div>
+          </MemberCard>
+        ))}
+      </MemberList>
+    </PageContainer>
+  );
 }
 const PageContainer = styled.div`
   text-align: center;
-  h2 {  
-font-family: Inter;
-font-size: 40px;
-font-weight: 700;
-line-height: 48px;
-text-align: center;
+
+  h2 {
+    margin: 0;
+    font-family: Inter;
+    font-size: 40px;
+    font-weight: 700;
+    line-height: 48px;
+    text-align: center;
   }
   p {
-
-font-family: Inter;
-font-size: 16px;
-font-weight: 400;
-line-height: 24px;
-text-align: center;
-
+    margin-bottom: 8px;
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    text-align: center;
+  }
+  .mainp {
+    margin-top: 40px;
+    font-family: Inter;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 26px;
+    text-align: center;
+    color: #3758f9;
+  }
+  .explanation {
+    margin-bottom: 60px;
   }
 `;
 
@@ -62,11 +85,20 @@ const MemberList = styled.div`
 `;
 
 const MemberCard = styled.div`
-  border: 1px solid #ccc;
-  padding: 20px;
-  border-radius: 8px;
-  width: 300px;
-  background-color: #f9f9f9;
+  .explanationbox {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 230px;
+    height: 83px;
+    background: #ffffff;
+    top: -110px;
+    left: 22px;
+    border-radius: 8px;
+    opacity: 0px;
+  }
 `;
 
 const MemberImage = styled.img`
@@ -76,10 +108,19 @@ const MemberImage = styled.img`
 `;
 
 const MemberName = styled.h3`
-  margin: 10px 0;
+  font-family: Inter;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: center;
+  margin: 1px;
 `;
 
 const MemberDescription = styled.p`
-  color: #666;
+  margin: 0;
+  font-family: Inter;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: center;
 `;
-
